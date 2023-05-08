7001e44edfbc4a3c656115c45300ba0e0148ac54
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-05-08 15:24:55 +0200

    fix button e img

--- JS/script.js
+++ JS/script.js
@@ -25,32 +25,28 @@ const images = [
 const containerHighlighted = document.querySelector('.highlighted');
 const containerThumbs = document.querySelector('.thumbs');
 
+for (let i = 0; i < images.length; i++) {
+	containerHighlighted.innerHTML += `<img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
+	containerThumbs.innerHTML += `<img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
+}
+
 
 const listHighlighted = document.querySelectorAll('.highlighted img');
 const listThumbs = document.querySelectorAll('.thumbs img');
 const btnPrev = document.querySelector('.btn-prev');
 const btnNext = document.querySelector('.btn-next');
 
-console.log (images.title);
 
-for (let i = 0; i < images.length; i++) {
-	containerHighlighted.innerHTML += `<img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
-	containerHighlighted.innerHTML += `<h1> ${images[i].title} </h1>`;
-	containerHighlighted.innerHTML += `<h3> ${images[i].text} </h3>`;
-	containerThumbs.innerHTML += `<img src="${images[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
-}
+let activeIndex = 0;
 
 btnNext.addEventListener('click',
 	function() {
-		// dall'immagine attiva tolgo la classe active
 		listHighlighted[activeIndex].classList.remove('active');
 		listThumbs[activeIndex].classList.remove('active');
-		// settiamo il nuovo valore di active index
 		activeIndex++;
 		if (activeIndex >= listHighlighted.length) {
 			activeIndex = 0;
 		}
-		// alla nuova immagine attiva aggiungiamo la classe active
 		listHighlighted[activeIndex].classList.add('active');
 		listThumbs[activeIndex].classList.add('active');
 	}
@@ -58,21 +54,17 @@ btnNext.addEventListener('click',
 
 btnPrev.addEventListener('click',
 	function() {
-		// dall'immagine attiva tolgo la classe active
 		listHighlighted[activeIndex].classList.remove('active');
 		listThumbs[activeIndex].classList.remove('active');
-		// settiamo il nuovo valore di active index
 		activeIndex--;
 		if (activeIndex < 0) {
 			activeIndex = listHighlighted.length - 1;
 		}
-		// alla nuova immagine attiva aggiungiamo la classe active
 		listHighlighted[activeIndex].classList.add('active');
 		listThumbs[activeIndex].classList.add('active');
 	}
 );
 
-// ciclo per aggiungere gli event listeners alle miniature
 for (let i = 0; i < listThumbs.length; i++) {
 	listThumbs[i].addEventListener('click',
 		function() {
